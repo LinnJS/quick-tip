2e5915a846999c2c507c83af7b75522f358926b3
Justin Linn <justinslinn727@gmail.com>
2018-07-21 19:40:17 -0400

    Inroduced native-base and eslint

--- App.js
+++ App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import { Button, Text, View } from 'react-native'
+import React from 'react'
+import Expo from 'expo'
+import {Text, View} from 'react-native'
 import styled from 'styled-components/native'
 
-const Container = styled.View`
+import {
+  Container, Content, Header, Left, Body, Title, Right, Icon, Button,
+} from 'native-base'
+
+const ScreenContainer = styled.View`
   align-items: center;
   background-color: papayawhip;
   flex: 1;
-  justify-content: center;
-  padding: 20px;
-`;
+`
 
- const BillAmountInput = styled.TextInput`
+const BillAmountInput = styled.TextInput`
   border: 1px solid aqua;
   height: 40px;
   width: 100%;
-`;
+`
 
 const ButtonWrapper = styled.View`
   flex-direction: row;
-`;
+`
 
 const CustomTip = styled.TextInput`
   border: 1px solid aqua;
@@ -26,66 +29,95 @@ const CustomTip = styled.TextInput`
   width: 60px;
   padding: 5px;
   margin: 5px;
-`;
+`
 
 export default class App extends React.Component {
-
   constructor() {
-    super();
+    super()
     this.state = {
       inputValue: '',
       tip: 0.2,
-    };
+      isReady: false,
+    }
   }
 
   updateCustomTip(customTip) {
     if (customTip) {
       this.setState({
         tip: parseFloat(customTip) / 100,
-      });
+      })
     } else {
-      this.setState({ tip: 0 });
+      this.setState({tip: 0})
     }
+  }
+
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      'Roboto': require('native-base/Fonts/Roboto.ttf'),
+      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+    });
+    this.setState({ isReady: true })
   }
 
   render() {
     let tip = 0.00
     if (this.state.inputValue) {
-      tip = parseFloat(this.state.inputValue) * this.state.tip;
-      tip = (Math.round(tip * 100) / 100).toFixed(2);
+      tip = parseFloat(this.state.inputValue) * this.state.tip
+      tip = (Math.round(tip * 100) / 100).toFixed(2)
     }
+
+    if (!this.state.isReady) {
+      return <Expo.AppLoading />
+    }
+
     return (
       <Container>
-          <Text>
-            ${tip}
-          </Text>
-        <BillAmountInput
-          keyboardType='numeric'
-          onChangeText={billAmount => this.setState({inputValue: billAmount})}
-          placeholder="0.00"
-          value={this.state.inputValue}
-        />
-        <ButtonWrapper>
-          <Button
-            onPress={() => this.setState({tip: 0.1})}
-            title="10%"
+        <Header>
+          <Left>
+            <Button transparent>
+              <Icon name="arrow-back"/>
+            </Button>
+          </Left>
+          <Body>
+          <Title>Header</Title>
+          </Body>
+          <Right>
+            <Button transparent>
+              <Icon name="menu"/>
+            </Button>
+          </Right>
+        </Header>
+        <Content padder>
+          <Text>${tip}</Text>
+          <BillAmountInput
+            keyboardType="numeric"
+            onChangeText={billAmount => this.setState({inputValue: billAmount})}
+            placeholder="0.00"
+            value={this.state.inputValue}
           />
-          <Button
-            onPress={() => this.setState({tip: 0.15})}
-            title="15%"
-          />
-          <Button
-            onPress={() => this.setState({tip: 0.2})}
-            title="20%"
-          />
-          <CustomTip
-            keyboardType='numeric'
-            onChangeText={customTip => this.setState({customTip: customTip})}
-            placeholder="20%"
-            value={(this.state.tip * 100).toString()}
-          />
-        </ButtonWrapper>
+          <ButtonWrapper>
+            <Button
+              onPress={() => this.setState({tip: 0.1})}>
+              <Text>10%</Text>
+            </Button>
+            <Button
+              onPress={() => this.setState({tip: 0.15})}
+            >
+              <Text>15%</Text>
+            </Button>
+            <Button
+              onPress={() => this.setState({tip: 0.2})}>
+              <Text>20%</Text>
+            </Button>
+            <CustomTip
+              keyboardType="numeric"
+              onChangeText={customTip => this.updateCustomTip(customTip)}
+              placeholder="20%"
+              value={(this.state.tip * 100).toString()}
+            />
+          </ButtonWrapper>
+        </Content>
       </Container>
-    );
+    )
   }
 }
